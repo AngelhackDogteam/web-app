a036ab0e509aa131e434b29f8fe25936ffe46ffe
Brandon Freiberg <freiberg.brandon@gmail.com>
2014-06-01 06:25:21 -0400

    wooba

--- dogteam/urls.py
+++ dogteam/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'dogteam.views.landing', name='landing'),
     url(r'^test_page$', 'dogteam.views.test_page', name='test_page'),
+    url(r'^recommend$', 'dogteam.views.recommend', name='recommend'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
